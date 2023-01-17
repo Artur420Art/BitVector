3aba073a4276af54c8ac55f0a30c03ebe9a8f98f
Arthur <92685845+Artur420Art@users.noreply.github.com>
2023-01-17 21:21:16 +0400

    Update bitvector.cpp

--- bitvector.cpp
+++ bitvector.cpp
@@ -16,14 +16,16 @@ void BitVector::set(const size_t pos, const int value)
 
     m_arr[pos] = static_cast<bool>(value);
 }
-int BitVector::operator[](size_t pos) {
+int BitVector::operator[](size_t pos) 
+{
     if (pos >= m_size) {
         throw std::invalid_argument("out of range!");
         return -1;
     }
     return m_arr[pos];
 }
-int BitVector::get(const size_t pos) {
+int BitVector::get(const size_t pos) 
+{
     if(pos >= m_size) {
         throw std::invalid_argument("out of range!");
         return -1;
@@ -31,33 +33,40 @@ int BitVector::get(const size_t pos) {
     return m_arr[pos];
 }
 
-BitVector::BitVector() {
+BitVector::BitVector() 
+{
     m_size = 32;
     m_arr = new int[m_size];
 }
-BitVector::BitVector(size_t size) {
+BitVector::BitVector(size_t size)
+{
     m_size = size;
     m_arr = new int[m_size];
 }
-BitVector::BitVector(size_t size, const int value) {
+BitVector::BitVector(size_t size, const int value) 
+{
     m_size = size;
     m_arr = new int[++m_size];
     m_arr[size] = static_cast<bool>(value);
 }
 
-BitVector::~BitVector() {
+BitVector::~BitVector()
+{
     delete [] m_arr;
 }
 
-BitVector::BitVector(const BitVector& rhs) {
+BitVector::BitVector(const BitVector& rhs) 
+{
     this->m_size = rhs.m_size;
     this->m_arr = new int[m_size];
-    for(int i = 0; i <= m_size; i++) {
+    for(int i = 0; i <= m_size; i++)
+    {
         this->m_arr[i] = rhs.m_arr[i];
     }
 }
 
-BitVector::BitVector(BitVector&& rhs) {
+BitVector::BitVector(BitVector&& rhs) 
+{
     this->m_size = rhs.m_size;
     this->m_arr = rhs.m_arr;
     rhs.m_arr = nullptr;
@@ -65,25 +74,31 @@ BitVector::BitVector(BitVector&& rhs) {
     
 }
 
-std::ostream& operator<<(std::ostream& os, const BitVector& rhs) {
-    for (int i = 0; i < rhs.m_size; ++i) {
+std::ostream& operator<<(std::ostream& os, const BitVector& rhs) 
+{
+    for (int i = 0; i < rhs.m_size; ++i) 
+    {
         os << rhs.m_arr[i] << " ";
     }
     return os;
 }
-BitVector& BitVector::operator=(const BitVector& rhs) {
-    if(this->m_size == rhs.m_size) {
+BitVector& BitVector::operator=(const BitVector& rhs)
+{
+    if(this->m_size == rhs.m_size)
+    {
         this->m_arr = rhs.m_arr;
         return *this;
     }
     this->m_size = rhs.m_size;
     this->m_arr = new int[m_size];
-    for(int i = 0; i <= m_size; i++) {
+    for(int i = 0; i <= m_size; i++) 
+    {
         this->m_arr[i] = rhs.m_arr[i];
     }
     return *this;
 }
-BitVector& BitVector::operator=(BitVector&& rhs) {
+BitVector& BitVector::operator=(BitVector&& rhs) 
+{
     this->m_size = rhs.m_size;
     this->m_arr = rhs.m_arr;
     rhs.m_arr = nullptr;
